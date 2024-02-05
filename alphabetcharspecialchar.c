18d76e61f7ceee18b058d743f98d8c6f1094582a
AAshishGit13 <koiralaashish27@gmail.com>
2024-02-05 20:31:11 +0530

    add to find special char number or alphabet

---
+++ alphabetcharspecialchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+ {
+    char ch;
+    printf("ENTER ANY CHARACTER: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("THE ALPHABET IS IN UPPER CASE");
+    } else if (ch >= 'a' && ch <= 'z') {
+        printf("THE ALPHABET IS IN LOWER CASE");
+    } else if (ch >= '0' && ch <= '9') {
+        printf("IT IS A NUMBER");
+    } else {
+        printf("IT IS A SPECIAL CHARACTER");
+    }
+
+    return 0;
+}
