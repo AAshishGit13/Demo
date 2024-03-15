beff5a8dfc2899466611b5cb07d0f6e7f4a831a7
AAshishGit13 <koiralaashish27@gmail.com>
2024-03-15 21:30:18 +0530

    maths

---
+++ maths.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 5;
+    
+    // Addition
+    int sum = a + b;
+    printf("Addition: %d + %d = %d\n", a, b, sum);
+    
+    // Subtraction
+    int difference = a - b;
+    printf("Subtraction: %d - %d = %d\n", a, b, difference);
+    
+    // Multiplication
+    int product = a * b;
+    printf("Multiplication: %d * %d = %d\n", a, b, product);
+    
+    return 0;
+}
